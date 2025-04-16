6bca84130f16644056bbc88b38f51683c5336e4b
Mathis Gaignet <m.gaignet@yahoo.fr>
2025-04-16 05:13:03 +0200

    Initial Commit

---
+++ BarTinder/BarTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarTinderApp.swift
+//  BarTinder
+//
+//  Created by Mathis Gaignet on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
