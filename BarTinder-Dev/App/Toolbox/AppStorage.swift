7cc4ad3ae7f5748f35c07a0b629825a65a2ab4eb
Mathis Gaignet <matgnt@icloud.com>
2025-10-20 15:41:17 +0200

    remove ObservableUserDefault

--- BarTinder-Dev/App/Toolbox/AppStorage.swift
+++ BarTinder-Dev/App/Toolbox/AppStorage.swift
@@ -5,16 +5,27 @@
 //  Created by Mathis Gaignet on 30/06/2025.
 //
 
-import ObservableUserDefault
 import Foundation
 
 @Observable
 final class Storage {
-    @ObservableUserDefault(.init(key: "has-fetched-cocktails", defaultValue: false, store: .standard))
-    @ObservationIgnored
-    var hasFetched: Bool
-    
-    @ObservableUserDefault(.init(key: "has-finished-swiping", defaultValue: false, store: .standard))
-    @ObservationIgnored
-    var hasFinshedSwiping: Bool
+    private let defaults = UserDefaults.standard
+
+    var hasFetched: Bool {
+        get {
+            defaults.bool(forKey: "has-fetched-cocktails")
+        }
+        set {
+            defaults.set(newValue, forKey: "has-fetched-cocktails")
+        }
+    }
+
+    var hasFinshedSwiping: Bool {
+        get {
+            defaults.bool(forKey: "has-finished-swiping")
+        }
+        set {
+            defaults.set(newValue, forKey: "has-finished-swiping")
+        }
+    }
 }
